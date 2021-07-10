a902a40661517cf608341f52f118044ed29b3976
unknown <aneeshsharmachd@gmail.com>
2021-07-11 00:06:20 +0530

    Setted up EJS, Views folder and home page ejs file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const port = 8500;
 const app = express();
 
+// setting up the server
 app.listen(port, function(err)
 {
     if (err)
@@ -14,4 +15,21 @@ app.listen(port, function(err)
     return; 
 });
 
+//returning response from the server
+// app.get('/',function(req,res)
+//     {
+//         return res.send("Message sent from the server");
+//     });    
+ 
 
+// setting up ejs template engine
+const path = require("path"); // reqiuiring the path
+app.set("view engine", "ejs"); // to set up ejs
+app.set("views" , path.join(__dirname, "views")); // to join the path of views directory with this server's path
+
+app.get('/',function(req,res)  //.get type of request is used on "/" url to perform the function 
+    {
+        return res.render("home");
+    });  
+
+    
